c0d6a448ea4e659c503efd14b4692f445ca21d93
Eric <eric.briand@gmail.com>
2020-02-20 22:14:37 +0100

    tayo!

--- sortLibraries.js
+++ sortLibraries.js
@@ -9,7 +9,11 @@ module.exports = function sortLibraries(libraries, ndays, bookScores) {
               return bookScores[book];
             })
             .reduce(reducer);
-    const indexA = (ndays - a.signupDuration) * aScores * a.shipCapacity;
+    const indexA =
+      a.books.length == 0
+        ? 0
+        : ((ndays - a.signupDuration) * aScores * a.shipCapacity) /
+          a.books.length;
     const bScores =
       b.books.length == 0
         ? 0
@@ -18,7 +22,11 @@ module.exports = function sortLibraries(libraries, ndays, bookScores) {
               return bookScores[book];
             })
             .reduce(reducer);
-    const indexB = (ndays - b.signupDuration) * bScores * b.shipCapacity;
+    const indexB =
+      b.books.length == 0
+        ? 0
+        : ((ndays - b.signupDuration) * bScores * b.shipCapacity) /
+          b.books.length;
     return indexB - indexA;
   });
 };
